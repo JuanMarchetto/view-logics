6014d3d601584bf8cc64c239a87a57359aa4e5f5
Juan Patricio Marchetto <juanpatriciomarchetto@gmail.com>
2021-04-12 23:20:05 -0300

    addressing issue #2

--- src/view-logics.js
+++ src/view-logics.js
@@ -1,14 +1,10 @@
 export const when = (condToView) =>
   condToView
     .filter((cond) => cond[0])
-    .map((cond, i) => {
-      return { ...cond[1], key: `${i}` };
-    });
+    .map((cond, i) => cond[1].key ? cond[1] : { ...cond[1], key: i });
 
 export const match = (condToView) =>
-  (condToView.find((cond) => cond[0])
-    ? condToView.find((cond) => cond[0])
-    : [, <></>])[1];
+  (condToView.find((cond) => cond[0]) ?? [, <></>])[1];
 
 export const times = (condToView) =>
   condToView
@@ -16,8 +12,5 @@ export const times = (condToView) =>
     .map((cond) =>
       Array.from(
         { length: typeof cond[0] === "number" ? parseInt(cond[0]) : 1 },
-        (v, i) => {
-          return { ...cond[1], key: `${i}` };
-        }
-      )
+        (v, i) => { return { ...cond[1], key: i }})
     );
